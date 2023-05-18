85e422025e7c0feebc305089c28133828870c812
Cole Hartkopp <chsoccer4@outlook.com>
2023-05-17 20:33:13 -0500

    server ready just need jwt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// database address once created
-mongoose.connect(process.env.MONGODB_URI || '');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ezleagues');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// const { jwtmiddleware } = require('./utils/auth');
 
-// require schemas, resolvers and typedefs
-// require connection to db
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    // typedefs
-    // resovlers,
-    // jwt middleware
+    typedefs,
+    resovlers,
+    // context: jwtmiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Set static path to client build folder
 
-// get route to grab build's index.html
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async () => {
     await server.start();
-    // apply/use jwt middleware
+    server.applyMiddleware({ app });
 
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`Server is running on ${PORT}!`);
+            console.log(`Use graphql at http://localhost:${PORT}${server.graphqlPath}`);
         })
     })
 };
