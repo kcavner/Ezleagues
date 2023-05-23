522483d6863002a02ba950385fc3832fc5f6e1c9
kcavner <kcavner@hotmail.com>
2023-05-23 13:05:00 -0500

    Revert "Front end"

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
 import Login from "./login"
 
-export default function Home() {
-  return (
-    <div>
-      {/* content here */}
-      <h1>Hello</h1>
-    </div>
-  )
-}
+function Navbar() {
+    const [currentPage, setCurrentPage] = useState('login');
+  
+    const handlePageChange = (page) => {
+      setCurrentPage(page);
+    };
+  
+    return (
+  <div>
+  <header>
+    <div className='name'style={{ cursor: "pointer" }} onClick={() => handlePageChange('login')}>EZLeagues</div>
+      <nav>
+       {/* a tag for page rendering, change the input for handlePageChange as needed */}
+            <a className='navLink' href="#" onClick={() => handlePageChange('login')}>
+              login
+            </a>
+      </nav>
+  </header>
+  
+    {currentPage === 'login' && <Login />}
+   </div>
+    );
+  }
+
+  export default Navbar
