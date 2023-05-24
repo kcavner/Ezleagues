019f190db91614873a2ab4e97d89c77e24daefff
Eli Pruneda <pruneda.eli@gmail.com>
2023-05-23 23:35:52 -0500

    Added styling to log in form, installed @tailwindcss/forms.

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -39,22 +39,41 @@ const Login = () =>{
     };
 
     return (
-        <div>
-        {/* <Header text = 'EZ Leagues'/> */}
-        <form onSubmit={handleSubmit}>
-          <h2>Login</h2>
-          <div>
-            <label>Email:</label>
-            <input type="email" value={email} onChange={handleEmailChange} required />
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
           </div>
-          <div>
-            <label>Password:</label>
-            <input type="password" value={password} onChange={handlePasswordChange} required />
-          </div>
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Email address:
+                  </label>
+                  <div className="mt-2">
+                    <input type="email" value={email} onChange={handleEmailChange} required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      Password
+                    </label>
+                  </div>
+                  <input type="password" value={password} onChange={handlePasswordChange} required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+                <div>
+                  <button type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Login
+                    </button>
+                </div>
+              </form>
+          </div>  
         </div>
       );
 }
