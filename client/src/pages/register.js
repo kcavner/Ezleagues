fc4ad967cccd8e9fceff3745967019d6a62c644a
Eli Pruneda <pruneda.eli@gmail.com>
2023-05-24 18:50:08 -0500

    Register page rework alternative.

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -63,19 +63,38 @@ const CreateUserForm = () => {
           <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+             Last Name:  
+            </label>
+            <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
-          <label>
-            Last Name:
-            <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-          </label>
-          <label>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
             User Name:
-            <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} required />
           </label>
-          <label>
+          <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} required
+           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+          
+        <div>
+          <div className="flex items-center justify-between">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
             Email:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required
+           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          </div>
+        </div>
+
           <label>
             Password:
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
