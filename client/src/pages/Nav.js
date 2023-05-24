c45b1377f978dafd9ae0b1c290ea0485f4d63d3c
unknown <kcavner@hotmail.com>
2023-05-24 13:25:47 -0500

    changes

--- client/src/pages/Nav.js
+++ client/src/pages/Nav.js
@@ -7,19 +7,37 @@ import { SlNotebook } from 'react-icons/sl';
 function Nav({ currentPage, handlePageChange }) {
     return(
         // add styling to this div below
-        <div className="fixed top-0 w-screen h-16 m-0
+        <div className="header-class fixed top-0 w-screen h-16 m-0
                         flex flex-row
                         bg-gray-700 text-white shadow-lg"> 
+        <div className='name'style={{ cursor: "pointer" }} onClick={() => handlePageChange('login')}>EZLeagues</div>
+            
+        <nav className="dropdown">
+            <ul>
+            <li>
+                <a className='navLink' href="#" onClick={() => handlePageChange('login')}>
+                login
+                </a>
+            </li>
+            <li>
+                <a className='navLink' href="#" onClick={() => handlePageChange('register')}>
+                Register
+                </a>
+            </li>
+            </ul>
+        </nav>
             {/* different navigation components here */}
             {/* login / logout */}
-            <NavIcon icon = {<SlLogin size="28"/>} text = 'Log In/Out' active={currentPage==='Login'}
-                onClick={()=> handlePageChange('Login')}/>
-            {/* home */}
-            <NavIcon icon = {<FiHome size="28"/>} text = 'Home' active={currentPage==='Home'}
-                onClick={()=> handlePageChange('Home')}/>
-            {/* register */}
-            <NavIcon icon={<SlNotebook size="28"/>} text = 'Register' active={currentPage==='Register'}
-                onClick={()=> handlePageChange('Register')}/>
+            <div className='icons'>
+                <NavIcon icon = {<SlLogin size="28"/>} text = 'Log In/Out' active={currentPage==='Login'}
+                    onClick={()=> handlePageChange('Login')}/>
+                {/* home */}
+                <NavIcon icon = {<FiHome size="28"/>} text = 'Home' active={currentPage==='Home'}
+                    onClick={()=> handlePageChange('Home')}/>
+                {/* register */}
+                <NavIcon icon={<SlNotebook size="28"/>} text = 'Register' active={currentPage==='Register'}
+                    onClick={()=> handlePageChange('Register')}/>
+            </div>
         </div>
     )
 }
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Login from "./login"
 import Register from "./register"
 
-function Navbar() {
+function Home() {
     const [currentPage, setCurrentPage] = useState('login');
   
     const handlePageChange = (page) => {
@@ -12,7 +12,7 @@ function Navbar() {
     return (
   <div>
   <header>
-    <div className='name'style={{ cursor: "pointer" }} onClick={() => handlePageChange('login')}>EZLeagues</div>
+    
       <nav>
        {/* a tag for page rendering, change the input for handlePageChange as needed */}
             <a className='navLink' href="#" onClick={() => handlePageChange('login')}>
@@ -24,10 +24,9 @@ function Navbar() {
       </nav>
   </header>
   
-    {currentPage === 'login' && <Login />}
-    {currentPage === 'register' && <Register />}
+    
    </div>
     );
   }
 
-  export default Navbar
+  export default Home
