cc46449402ec55744b048eca57e03a5835e86f38
Eli Pruneda <pruneda.eli@gmail.com>
2023-05-25 17:45:57 -0500

    Fixed formatting for teams.

--- client/src/pages/Teams.js
+++ client/src/pages/Teams.js
@@ -12,7 +12,7 @@ export default function Teams() {
     if (error) return <p>Error: {error.message}</p>;
 
     return (
-        <div className="flex flex-wrap justify-center align-center p-10">
+        <div className="flex flex-col justify-center align-center p-10">
             {teams.map(team => (
             <BoxComponent
                 key={team.name}
@@ -26,20 +26,19 @@ export default function Teams() {
 
 const BoxComponent = ({ name, roster }) => {
     return (
-        <div className="position relative w-80 h-100
-        flex border mb-2.5 p-2.5 border-solid border-[#ccc]">
+        <div className="relative w-4/5 h-full flex flex-col border mb-2.5 p-2.5 border-solid border-[#ccc]">
           <span></span>
           <div className="content m-4">
             {/* sport name */}
             <h2>Team Name:{name}</h2> 
             {/* sport desc */}
-            <h3>Roster:{roster}</h3>
+            <h3>Roster:</h3>
             <ul>
                 {roster.map((playerId) =>(
                     <li key={playerId}>{playerId}</li>
                 ))}
             </ul>
-            <button>Details</button>
+            {/* <button>Details</button> */}
           </div>
         </div>
       );
