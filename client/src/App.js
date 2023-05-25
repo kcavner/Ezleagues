cba18d938bf2ce86e8354a2fd6b016c69a6d1ccc
Cole Hartkopp <chsoccer4@outlook.com>
2023-05-25 17:49:14 -0500

    User page created and user query updated

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ import Container from './pages/Container';
 const httpLink = createHttpLink({
    //for local deployment use 'http://localhost:3001/graphql', for live deployment use '/graphql'
 
-  uri: '/graphql'
-  // uri: 'http://localhost:3001/graphql'
+  // uri: '/graphql'
+  uri: 'http://localhost:3001/graphql'
 });
 
 // Auth middleware that attaches JWT to every request as an Auth header
