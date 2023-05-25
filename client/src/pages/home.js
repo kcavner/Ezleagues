f371774aa51a5a36dd5a4551fa0ed8d0524a2545
Cole Hartkopp <chsoccer4@outlook.com>
2023-05-25 12:15:40 -0500

    pages render on dashboard

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Auth from '../utils/auth'
 import Organization from './Organization';
+import Sports from "./Sports";
+import Teams from "./Teams";
+import Match from "./Match";
 
 function Home() {
   const [currentPage, setCurrentPage] = useState('');
@@ -15,7 +18,20 @@ if (Auth.loggedIn() === true){
       </div>;
   } else if (user.data.isPlayer === true) {
     // Render content for player
-    
+    const renderPage = () => {
+      if (currentPage === 'Organization'){
+        return <Organization />
+    }
+    if (currentPage === 'Sports'){
+        return <Sports />;
+    }
+    if (currentPage === 'Teams'){
+        return <Teams />;
+    }
+    if (currentPage === 'Match'){
+        return <Match />
+    }
+    }
     return   <div className='home-dash-container'>
     <div className='home-dash'>
 <button className='home-button' onClick={() => handlePageChange('Organization')}>Organizations</button>
@@ -27,7 +43,7 @@ if (Auth.loggedIn() === true){
 <button className='home-button' onClick={() => handlePageChange('Match')}>Get Matches</button>
 
 <button className='home-button'>Get Users</button>
-{currentPage === 'Organization' && <Organization />}
+{renderPage()}
       </div>
     </div>
   } else if (user.isCaptain === true) {
