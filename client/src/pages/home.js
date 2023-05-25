c1b73dd98adb7b964609fe871f0c94811d2d5e40
Cole Hartkopp <chsoccer4@outlook.com>
2023-05-24 20:51:24 -0500

    redirect to page is hard

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import Auth from '../utils/auth'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Organization from './Organization';
 
 function Home() {
-  
+  // const navigate = useNavigate();
+  // const navigateToOrgs = () => {
+  //   navigate('/Organization');
+  // };
+  const [currentPage, setCurrentPage] = useState('');
+  const handlePageChange = (page) => setCurrentPage(page);
 if (Auth.loggedIn() === true){
   const user = Auth.getUser();
   console.log(user)
@@ -14,7 +21,7 @@ if (Auth.loggedIn() === true){
   } else if (user.data.isPlayer === true) {
     // Render content for player
     return <div className='home-dash'>
-<button className='home-button'>Organizations</button>
+<button className='home-button' onClick={() => handlePageChange('Organization')}>Organizations</button>
 
 <button className='home-button'>Sports</button>
 
@@ -23,6 +30,7 @@ if (Auth.loggedIn() === true){
 <button className='home-button'>Get Matches</button>
 
 <button className='home-button'>Get Users</button>
+{currentPage === 'Organization' && <Organization />}
       </div>;
   } else if (user.isCaptain === true) {
     // Render content for captain
